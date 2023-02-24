75818f39d37c84e18dd305c2fc0dde53078e799c
frma <frma059@gmail.com>
2023-02-24 00:15:41 -0300

    inicialização

---
+++ arrays/longestPeak.py
@@ -0,0 +1,107 @@
+def longestPeak(array):
+    peak = []
+    maxLength = 0
+
+    peakStart = True
+    # peakEnd = False
+
+    increasing = False
+    increased = False
+
+    for index, value in enumerate(array):
+        if index < len(array) - 1:
+            if value == array[index + 1]:
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                peakStart = True
+                peak = []
+                continue
+
+            if array[index + 1] > value:
+                increasing = True
+
+            else:
+                increasing = False
+
+            if index != 0:
+                if array[index - 1] < value:
+                    increased = True
+
+                else:
+                    increased = False
+
+            if increasing and peakStart:
+                peak.append(value)
+                continue
+
+            elif increasing and not peakStart:
+                peak.append(value)
+                peakStart = True
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                peak = [value]
+                continue
+
+            elif not increasing and peakStart:
+                if increased:
+                    peakStart = False
+                    peak.append(value)
+                    continue
+
+                else:
+                    peakStart = True
+                    if len(peak) > maxLength:
+                        maxLength = len(peak)
+                    peak = []
+                    continue    
+
+            elif not increasing and not peakStart:
+                peakStart = False
+                peak.append(value)
+                continue
+
+        else:
+
+            if value == array[index - 1]:
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                peakStart = True
+                peak = []
+                continue
+
+            print(f'valor anterior: ${array[index - 1]}')
+
+            if array[index - 1] < value:
+                increased = True
+
+            else:
+                increased = False
+
+            if increased and peakStart:
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                peak = []
+                continue
+
+            elif increased and not peakStart:
+                peakStart = True
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                peak = []
+                continue
+
+            elif not increased and peakStart:            
+                peakStart = True
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                peak = []
+                continue
+
+            elif not increased and not peakStart:
+                peakStart = False
+                peak.append(value)
+                if len(peak) > maxLength:
+                    maxLength = len(peak)
+                continue
+
+    return maxLength
---
+++ arrays/run.py
@@ -0,0 +1,7 @@
+from longestPeak import longestPeak as lp
+
+array = [5, 4, 3, 2, 1, 2, 1]
+
+longestPeak = lp(array)
+
+print(longestPeak)
