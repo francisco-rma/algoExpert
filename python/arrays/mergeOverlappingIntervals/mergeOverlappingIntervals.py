cd3884826564cc0e8c57ba203708d8235c550a05
frma <frma059@gmail.com>
2023-03-23 23:00:52 -0300

    commit pra poder testar swift

---
+++ python/arrays/mergeOverlappingIntervals/mergeOverlappingIntervals.py
@@ -0,0 +1,10 @@
+def mergeOverlappingIntervals(intervals):
+    i = 0
+    # mergedArray = []
+    while i < len(intervals):
+        # checkingArray = []
+        # candidates = [interval for interval in intervals if interval[0] < 1]
+
+        break
+
+    return [[]]
