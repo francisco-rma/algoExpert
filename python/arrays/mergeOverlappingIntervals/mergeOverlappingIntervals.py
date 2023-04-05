1b7ad409b0cfaf6bf9f7c8e1847f1edf174603d7
frma <frma059@gmail.com>
2023-04-04 21:38:49 -0300

    base

--- python/arrays/mergeOverlappingIntervals/mergeOverlappingIntervals.py
+++ python/arrays/mergeOverlappingIntervals/mergeOverlappingIntervals.py
@@ -1,10 +1,50 @@
 def mergeOverlappingIntervals(intervals):
-    i = 0
-    # mergedArray = []
-    while i < len(intervals):
-        # checkingArray = []
-        # candidates = [interval for interval in intervals if interval[0] < 1]
 
-        break
+    final_intervals_dict = {}
+    final_intervals = []
 
-    return [[]]
+    original_size = len(intervals)
+    final_intervals_dict[original_size] = intervals.pop(0)
+
+    i = len(intervals)
+
+    while i > 0:
+        interval = intervals.pop(0)
+        i = len(intervals)
+
+        merged = False
+
+        for key, value in final_intervals_dict.items():
+
+            final_max = value[len(value) - 1]
+            final_min = value[0]
+
+            overlaps: bool = (final_min <= interval[0] and interval[0] <= final_max) or (
+
+                final_min <= interval[len(interval) - 1] and interval[len(interval) - 1] <= final_max) or (
+
+                interval[len(interval) - 1] > final_max and interval[0] < final_min)
+
+            if overlaps:
+                if interval[0] < final_min:
+                    final_min = interval[0]
+
+                if interval[len(interval) - 1] > final_max:
+                    final_max = interval[len(interval) - 1]
+
+                final_interval = [final_min, final_max]
+                final_intervals_dict[key] = final_interval
+
+                merged = True
+                break
+
+        if merged:
+            continue
+
+        else:
+            final_intervals_dict[i] = interval
+
+    for key, value in final_intervals_dict.items():
+        final_intervals.append(value)
+
+    return final_intervals
---
+++ python/arrays/mergeOverlappingIntervals/run.py
@@ -0,0 +1,10 @@
+from mergeOverlappingIntervals import mergeOverlappingIntervals as moi
+
+array = [
+    [1, 22],
+    [-20, 30]
+]
+
+longestPeak = moi(array)
+
+print(longestPeak)
